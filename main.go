3879c2ac638047e4ead9a75c746422d87b5f050f
gomoot <34185272+gomoot@users.noreply.github.com>
2019-03-03 08:31:20 +0700

    Update test log

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/natefinch/lumberjack"
+	//"github.com/sirupsen/logrus"
+)
+
+var errLog *log.Logger
 
 func main() {
-	fmt.Println("aaaaaaaaa")
 
+	e, err := os.OpenFile("./tt.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+		os.Exit(1)
+	}
+	errLog = log.New(e, "", log.Ldate|log.Ltime)
+	errLog.SetOutput(&lumberjack.Logger{
+		Filename:   "./foo.log",
+		MaxSize:    100,   // megabytes after which new file is created
+		MaxBackups: 10000, // number of backups
+		MaxAge:     28,    //days
+	})
+
+	for i := 0; i <= 100000000; i++ {
+		if i % 4 == 0 {
+		errLog.Printf("ddddddddddddddddddddddddddddddddTest Test\n")
+		} else {
+			errLog.Printf("ddddddddddddddddddddddddddiohdfjjjjjjjjjjjjjjjjjjjjjjjjjjjjj;lkdj kaddddddTest Test\n")
+			
+		}
+	}
 }
